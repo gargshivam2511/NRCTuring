d3bb0c83824a78135d3703f306482f0a84ed6f41
ycheng56 <cynthiacheng95@gmail.com>
2022-03-30 16:06:08 -0700

    add Screens

--- components/AdminScreen.js
+++ components/AdminScreen.js
@@ -1,20 +1,24 @@
 import React, { Component } from "react";
-import { Text, View } from "react-native";
+import { Text, View, Image, Pressable, TouchableOpacity } from "react-native";
 import styles from "./styles";
 
 export default class AdminScreen extends Component {
   constructor(props) {
     super(props);
   }
-  state = {
-    answers: [],
-    key: 6,
-  };
+  state = {};
   render() {
     const { route, navigation } = this.props;
     return (
       <View style={styles.container}>
         <Text>Admin Screen</Text>
+        <Pressable
+          style={styles.button}
+          onPress={() => {alert("to be built");
+          }}
+        >
+          <Text style={styles.text}>Upload</Text>
+        </Pressable>
       </View>
     );
   }
