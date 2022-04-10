c7882c5f674e874597df01085b981d54fcfdb512
weifeng-bit <baiweifeng1992@gmail.com>
2022-04-09 17:24:55 -0700

    implmented reading 10 questions in order and added original english sentence onto screen

--- components/GameScreen.js
+++ components/GameScreen.js
@@ -17,46 +17,19 @@ import human from "../assets/Human.png";
 import neuro from "../assets/Neuro.png";
 import staty from "../assets/Staty.png";
 import styles from "./styles";
+import getData from "./Util.js";
+
+let humanPoint = 0;
+let robotPoint = 0;
+let logs = [];
 
 export default function GameScreen({ route, navigation }) {
-  const [questions, setQuestions] = useState([
-    {
-      id: 1,
-      original:
-        "La Maison Blanche fait pression pour que des inspecteurs nucléaires soient envoyés dès que possible pour surveiller la fermeture par la Corée du Nord de ses réacteurs nucléaires",
-      englishReference:
-        "White House Pushes for Nuclear Inspectors to Be Sent as Soon as Possible to Monitor North Korea's Closure of Its Nuclear Reactors",
-      humanTranslation:
-        "White House Urges Hastily Sending Nuclear Inspectors to Supervise North Korea's Nuclear Reactor Shutdown",
-      humanScore: 0.91271,
-      neuralTranslation:
-        "White House is pushing for nuclear inspectors to be sent as soon as possible to monitor North Korea's closure of its nuclear reactors.",
-      nerualScore: 0.957052,
-      statisticalTranslation:
-        "The White House pushed to nuclear inspectors be sent as soon as possible to oversee the closure of North Korea's nuclear reactors.",
-      statisticalScore: 0.935441,
-    },
-    {
-      id: 2,
-      original:
-        "Les experts estiment que le plutonium élémentaire que Pyongyang a produit au cours des 20 dernières années est suffisant pour créer douze armes nucléaires.",
-      englishReference:
-        "Experts believe that the elemental plutonium that Pyongyang has produced over the last 20 years is sufficient to create twelve nuclear weapons.",
-      humanTranslation:
-        "Experts believe that the plutonium produced by Pyongyang in the last 20 years is enough to manufacture 12 nuclear weapons.",
-      humanScore: 0.968453,
-      neuralTranslation:
-        "Experts believe that the basic plutonium produced by Pyongyang over the past 20 years is sufficient to create 12 nuclear weapons.",
-      nerualScore: 0.980668,
-      statisticalTranslation:
-        "Experts estimate that plutonium basic Pyongyang has produced over the last 20 years is enough to create 12 nuclear weapons.",
-      statisticalScore: 0.968812,
-    },
-  ]);
+  const [questions, setQuestions] = useState([]);
+  const [options, setOptions] = useState([]);
+  const [scores, setScores] = useState([]);
+  const [images, setImages] = useState([suspect1, suspect2, suspect3]);
+  const [imgOrder, setImgOrder] = useState([]);
   const [key, setKey] = useState(6);
-  const [image1, setImage1] = useState(suspect1);
-  const [image2, setImage2] = useState(suspect2);
-  const [image3, setImage3] = useState(suspect3);
   const [showScore, setShowScore] = useState(false);
   const [buttonText, setButtonText] = useState("Submit");
   const [currentQuestion, setCurrentQuestion] = useState(0);
@@ -64,9 +37,46 @@ export default function GameScreen({ route, navigation }) {
   const [pressOption, setPressOption] = useState(initialState);
   const [lockOption, setLockOption] = useState(false);
   const { image } = route.params;
+  const [humanScore, setHumanScore] = useState(0);
+  const [robotScore, setRobotScore] = useState(0);
+
   useEffect(() => {
-    //console.log("In useEffect");
-  }, [currentQuestion]);
+    setQuestions(getData(10));
+  }, []);
+
+  const updateOptions = () => {
+    const currQ = questions[currentQuestion];
+
+    let tempOps = [];
+    let tempScores = [];
+    let tempImgs = [];
+    for (let i = 0; i < 3; i++) {
+      if (currQ.key_human === i) {
+        tempOps.push(currQ.trans_human);
+        tempScores.push(currQ.score_human);
+        tempImgs.push(human);
+      }
+      if (currQ.key_neural === i) {
+        tempOps.push(currQ.trans_neural);
+        tempScores.push(currQ.score_neural);
+        tempImgs.push(neuro);
+      }
+      if (currQ.key_stat === i) {
+        tempOps.push(currQ.trans_stat);
+        tempScores.push(currQ.score_stat);
+        tempImgs.push(staty);
+      }
+      setImages(tempImgs);
+      setOptions(tempOps);
+      setScores(tempScores);
+    }
+  };
+
+  useEffect(() => {
+    if (currentQuestion < questions.length) {
+      updateOptions();
+    }
+  }, [questions, currentQuestion]);
 
   const onSubmitNextPress = () => {
     if (currentQuestion >= questions.length) {
@@ -77,10 +87,32 @@ export default function GameScreen({ route, navigation }) {
     }
     if (buttonText == "Submit") {
       if (pressOption[0] || pressOption[1] || pressOption[2]) {
-        setImage1(human);
-        setImage2(neuro);
-        setImage3(staty);
         setShowScore(true);
+
+        for (let i = 0; i < 3; i++) {
+          if (pressOption[i] && questions[currentQuestion].key_human === i) {
+            setHumanScore(humanScore + 1);
+            humanPoint = humanScore;
+          }
+        }
+
+        if (
+          questions[currentQuestion].score_human >=
+            questions[currentQuestion].score_neural &&
+          questions[currentQuestion].score_human >=
+            questions[currentQuestion].score_stat
+        ) {
+          setRobotScore(robotScore + 1);
+          robotPoint = robotScore;
+        }
+        logs.push(
+          questions[currentQuestion].id +
+            ", " +
+            questions[currentQuestion].orig_fr +
+            ", " +
+            questions[currentQuestion].key_human
+        );
+
         setButtonText("Next");
         setLockOption(true);
       } else {
@@ -91,9 +123,7 @@ export default function GameScreen({ route, navigation }) {
     } else {
       setCurrentQuestion(currentQuestion + 1);
       setLockOption(false);
-      setImage1(suspect1);
-      setImage2(suspect2);
-      setImage3(suspect3);
+      setImages(initialState);
       setShowScore(false);
       setButtonText("Submit");
       setPressOption(initialState);
@@ -117,16 +147,23 @@ export default function GameScreen({ route, navigation }) {
     const question = questions[currentQuestion];
     return (
       <SafeAreaView style={styles.questionContainer}>
-        <Image source={image} style={{width:40,height:40,borderRadius:50}} />
+        <Image
+          source={image}
+          style={{ width: 40, height: 40, borderRadius: 50 }}
+        />
         {question ? (
           <>
-            <Text style={styles.question}>Question {question.id}</Text>
+            <Text style={styles.question}>Question {currentQuestion + 1}</Text>
             <View style={styles.innerContainer}>
-              <Text style={styles.question}>Sentence: {question.original}</Text>
+              <Text style={styles.question}>French: {question.orig_fr}</Text>
+              <Text style={styles.question}>English: {question.gold_en}</Text>
             </View>
 
             <View style={styles.innerContainer}>
-              <Image source={image1} style={styles.image} />
+              <Image
+                source={showScore ? images[0] : suspect1}
+                style={styles.image}
+              />
               <TouchableOpacity
                 style={[
                   styles.option,
@@ -136,15 +173,18 @@ export default function GameScreen({ route, navigation }) {
                 ]}
                 onPress={() => pressOneOption(0)}
               >
-                <Text>{question.humanTranslation}</Text>
+                <Text>{question.trans_human}</Text>
               </TouchableOpacity>
               {showScore && (
-                <Text style={styles.score}>Score: {question.humanScore}</Text>
+                <Text style={styles.score}>Score: {question.score_human}</Text>
               )}
             </View>
 
             <View style={styles.innerContainer}>
-              <Image source={image2} style={styles.image} />
+              <Image
+                source={showScore ? images[1] : suspect2}
+                style={styles.image}
+              />
               <TouchableOpacity
                 style={[
                   styles.option,
@@ -154,15 +194,18 @@ export default function GameScreen({ route, navigation }) {
                 ]}
                 onPress={() => pressOneOption(1)}
               >
-                <Text>{question.neuralTranslation}</Text>
+                <Text>{question.trans_neural}</Text>
               </TouchableOpacity>
               {showScore && (
-                <Text style={styles.score}>Score: {question.nerualScore}</Text>
+                <Text style={styles.score}>Score: {question.score_neural}</Text>
               )}
             </View>
 
             <View style={styles.innerContainer}>
-              <Image source={image3} style={styles.image} />
+              <Image
+                source={showScore ? images[2] : suspect3}
+                style={styles.image}
+              />
               <TouchableOpacity
                 style={[
                   styles.option,
@@ -172,12 +215,10 @@ export default function GameScreen({ route, navigation }) {
                 ]}
                 onPress={() => pressOneOption(2)}
               >
-                <Text>{question.statisticalTranslation}</Text>
+                <Text>{question.trans_stat}</Text>
               </TouchableOpacity>
               {showScore && (
-                <Text style={styles.score}>
-                  Score: {question.statisticalScore}
-                </Text>
+                <Text style={styles.score}>Score: {question.score_stat}</Text>
               )}
             </View>
           </>
@@ -194,3 +235,13 @@ export default function GameScreen({ route, navigation }) {
 
   return <>{showOneQuestion()}</>;
 }
+
+const getHumanScore = () => {
+  return humanPoint;
+};
+
+const getRobotScore = () => {
+  return robotPoint;
+};
+
+export { getHumanScore, getRobotScore };
