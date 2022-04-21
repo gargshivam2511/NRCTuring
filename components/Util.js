ba71e828aaeed7eb2bbc2ae45736e4e0d673330d
modhpranav <modh.p@northeastern.edu>
2022-04-20 21:20:35 -0700

    fixed ios log handling

--- components/Util.js
+++ components/Util.js
@@ -1,24 +1,35 @@
 import * as FileSystem from 'expo-file-system';
+import * as Sharing from "expo-sharing";
 import { getLogs, setLogs } from './GameScreen';
 
 const saveFile = async () => {
-
-    var albumName = "NRCTuring";
-    const albumUri = FileSystem.StorageAccessFramework.getUriForDirectoryInRoot(albumName);
-
-    const permissions = await FileSystem.StorageAccessFramework.requestDirectoryPermissionsAsync(albumUri);
-    if (!permissions.granted) {
-        return;
+    
+    if (isIos) {
+        var filename = "gamelog" + "_" + Date.now() + ".tsv"
+        let fileUri = FileSystem.documentDirectory + filename
+        await FileSystem.writeAsStringAsync(fileUri, getLogs(), { encoding: FileSystem.EncodingType.UTF8 });
+        setLogs();
+        const UTI = 'public.item';
+        const shareResult = await Sharing.shareAsync(downloadedFile.uri, {UTI});
     }
-    const permittedUri = permissions.directoryUri;
-    if (!permittedUri.includes(albumName)) {
-        console.log("Permission Not Found for NRCTuring directory at root level");
-        return;
+    else {
+        var albumName = "NRCTuring";
+        const albumUri = FileSystem.StorageAccessFramework.getUriForDirectoryInRoot(albumName);
+
+        const permissions = await FileSystem.StorageAccessFramework.requestDirectoryPermissionsAsync(albumUri);
+        if (!permissions.granted) {
+            return;
+        }
+        const permittedUri = permissions.directoryUri;
+        if (!permittedUri.includes(albumName)) {
+            console.log("Permission Not Found for NRCTuring directory at root level");
+            return;
+        }
+        let fileUri = await FileSystem.StorageAccessFramework.createFileAsync(albumUri,
+            "gamelog" + "_" + Date.now() + ".tsv", "text/plain");
+        await FileSystem.StorageAccessFramework.writeAsStringAsync(fileUri, getLogs(), { encoding: FileSystem.EncodingType.UTF8 });
+        setLogs();
     }
-    let fileUri = await FileSystem.StorageAccessFramework.createFileAsync(albumUri,
-        "gamelog" + "_" + Date.now() + ".tsv", "text/plain");
-    await FileSystem.StorageAccessFramework.writeAsStringAsync(fileUri, getLogs(), { encoding: FileSystem.EncodingType.UTF8 });
-    setLogs();
 }
 
 export default saveFile
